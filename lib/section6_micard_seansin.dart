988821d1d86cd239fc023028e0128852654d499c
SeanSin <seansin@chungbuk.ac.kr>
2020-04-03 16:37:04 +0900

    Udemy Section 6 - 48 CircleAvatar & CustomFont 실습코드 작성완료

---
+++ lib/section6_micard_seansin.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+                backgroundImage: NetworkImage(
+                    'https://avatars2.githubusercontent.com/u/22961251?s=460&u=46a0fd8b5a539d1ab124481114c984585bc3d00a&v=4'),
+              ),
+              Text(
+                'Sean Sin',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color: Colors.teal[100],
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
