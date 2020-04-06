390b0d3aaef14e6eeee9b0d774516e68270b9305
SeanSin <seansin@chungbuk.ac.kr>
2020-04-06 15:22:10 +0900

    Udemy Section 7- 66. Challenge Complete Code 작성

--- lib/section7_dicee_seansin.dart
+++ lib/section7_dicee_seansin.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show Random;
 
 void main() => runApp(DiceApp());
+
+typedef void MyCallback(int foo);
 
 class DiceApp extends StatelessWidget {
   @override
@@ -18,7 +21,25 @@ class DiceApp extends StatelessWidget {
   }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State createState() => _RandomDiceGenerator();
+}
+
+class _RandomDiceGenerator extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 1;
+
+  void generateRandomNumber(String str) {
+    final _random = new Random.secure();
+    int next(int min, int max) => min + _random.nextInt(max - min);
+    if (identical('left', str)) {
+      setState(() => leftDiceNum = next(1, 7));
+    } else {
+      setState(() => rightDiceNum = next(1, 7));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -26,18 +47,14 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice1.png'),
-              // VoidCallback
-              onPressed: () {
-                print('Left button got pressed.');
-              },
+              child: Image.asset('images/dice$leftDiceNum.png'),
+              onPressed: () => generateRandomNumber('left'),
             ),
           ),
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice2.png'),
-              // VoidCallback
-              onPressed: () => print('Right button got pressed.'),
+              child: Image.asset('images/dice$rightDiceNum.png'),
+              onPressed: () => generateRandomNumber('right'),
             ),
           ),
         ],
