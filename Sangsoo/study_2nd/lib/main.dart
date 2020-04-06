b8a6acf71d01b31fcad499a515fae4f5da718630
sangsooleea <dulsang@naver.com>
2020-04-06 11:13:14 +0900

    7강

--- Sangsoo/study_2nd/lib/main.dart
+++ Sangsoo/study_2nd/lib/main.dart
@@ -12,34 +12,15 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              width: 100,
-              color: Colors.red,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.yellow,
-                ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.green,
-                )
-              ],
-            ),
-            Container(
-                width: 100,
-                color: Colors.blue,
-            )
-          ],
-        )),
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.redAccent,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
