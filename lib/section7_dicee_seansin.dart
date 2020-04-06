892eb0b21ba510673bd9f230672f5e24d53ef668
SeanSin <seansin@chungbuk.ac.kr>
2020-04-06 11:51:01 +0900

    Udemy Section 7- 54. Image 클래스에 Padding 추가 및 Center Widget 추가

--- lib/section7_dicee_seansin.dart
+++ lib/section7_dicee_seansin.dart
@@ -21,18 +21,23 @@ class DiceApp extends StatelessWidget {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          //리팩토링
-          child: Image.asset('mages/dice1.png'),
-        ),
-        Expanded(
-          child: Image(
-            image: AssetImage('images/dice2.png'),
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
           ),
-        ),
-      ],
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
