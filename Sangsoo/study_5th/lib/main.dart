672df72cf3b08d5cccc91e5bc38ad8c1d39df04c
sangsooleea <dulsang@naver.com>
2020-04-07 15:33:32 +0900

    before loop

--- Sangsoo/study_5th/lib/main.dart
+++ Sangsoo/study_5th/lib/main.dart
@@ -1,15 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(){
+    final player = AudioCache();
+    player.play('note1.wav');
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
-        ),
+            child: Column(
+          children: <Widget>[
+            FlatButton(
+              color: Colors.red,
+              onPressed: () {
+                playSound();
+              },
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound();
+              },
+              color: Colors.orange,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound();
+              },
+              color: Colors.yellow,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound();
+              },
+              color: Colors.green,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound();
+              },
+              color: Colors.blue,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound();
+              },
+              color: Colors.indigo,
+            ),
+            FlatButton(
+              onPressed: () {
+                playSound();
+              },
+              color: Colors.purple,
+            ),
+          ],
+        )),
       ),
     );
   }
