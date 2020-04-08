7d4a0719f8c16657fde4b0332abf5aa6b446a4c5
sangsooleea <dulsang@naver.com>
2020-04-08 14:20:33 +0900

    'question 출력, 버튼기능'

--- Sangsoo/study_6th/lib/main.dart
+++ Sangsoo/study_6th/lib/main.dart
@@ -24,18 +24,17 @@ class QuizPage extends StatefulWidget {
   _QuizPageState createState() => _QuizPageState();
 }
 
-List<Icon> scoreKeeper = [
-  Icon(
-    Icons.check,
-    color: Colors.green,
-  ),
-  Icon(
-    Icons.close,
-    color: Colors.red,
-  )
+List<Icon> scoreKeeper = [];
+List<String> question = [
+  'You can lead a cow down stairs but not up stairs.',
+  'Approximately one quarter of human bones are in the feet.',
+  'A slug\'s blood is green.'
 ];
 
 class _QuizPageState extends State<QuizPage> {
+  int maxSizeofQuestion = question.length;
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -48,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                question[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -74,6 +73,10 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 setState(() {
                   scoreKeeper.add(Icon(Icons.check, color: Colors.blue));
+                  if (questionNumber < maxSizeofQuestion - 1)
+                    questionNumber++;
+                  else
+                    questionNumber = 0;
                 });
               },
             ),
@@ -105,9 +108,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
