ff22acde0578c967c30555003855b0580bdacb63
SeanSin <seansin@chungbuk.ac.kr>
2020-04-09 14:44:13 +0900

    Udemy Section 11 - Completed Refactoring && Practice

--- lib/section11/story_brain.dart
+++ lib/section11/story_brain.dart
@@ -2,6 +2,7 @@ import 'package:mi_card/section11/story.dart';
 
 class StoryBrain {
   int _storyNumber = 0;
+
   List<Story> _storyData = [
     Story(
         storyTitle:
@@ -46,27 +47,33 @@ class StoryBrain {
     return _storyData[_storyNumber].getChoice2();
   }
 
-  void nextStory(int choiceNum) {
-    if (_storyNumber == 0 && choiceNum == 1) {
-      _storyNumber = 2;
-    } else if (_storyNumber == 0 && choiceNum == 2) {
-      _storyNumber = 1;
-    } else if (_storyNumber == 1 && choiceNum == 1) {
-      _storyNumber = 2;
-    } else if (_storyNumber == 1 && choiceNum == 2) {
-      _storyNumber = 3;
-    } else if (_storyNumber == 2 && choiceNum == 1) {
-      _storyNumber = 5;
-    } else if (_storyNumber == 2 && choiceNum == 2) {
-      _storyNumber = 4;
+  void storyHandler(int choiceNum) {
+    _storyNumber = storyNavigator(_storyNumber, choiceNum);
+  }
+
+  int restart() {
+    return 0;
+  }
+
+  int storyNavigator(int storyNum, int choiceNum) {
+    if (storyNum == 0) {
+      return (choiceNum == 1) ? 2 : 1;
+    }
+    if (storyNum == 1) {
+      return (choiceNum == 1) ? 2 : 3;
+    }
+    if (storyNum == 2) {
+      return (choiceNum == 1) ? 5 : 4;
+    } else {
+      return restart();
+    }
+  }
+
+  bool buttonShouldBeVisible() {
+    if (_storyNumber < 3) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
-
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
-
-//TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
-
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
-
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
