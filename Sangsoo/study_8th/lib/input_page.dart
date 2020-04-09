262cab4b771aa022bce6ae0c7124629571c463b0
sangsooleea <dulsang@naver.com>
2020-04-09 14:34:47 +0900

    'input_pages 분할'

---
+++ Sangsoo/study_8th/lib/input_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Container(
+        color: Color(0xFF1D1E33),
+        height: 200.0,
+        width:170.0
+      ),
+    );
+  }
+}
