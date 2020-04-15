0cd26584652f4a6725d715820220616bd3cd60ff
sangsooleea <dulsang@naver.com>
2020-04-15 12:14:15 +0900

    15강 스타트

--- Sangsoo/study_9th/lib/screens/loading_screen.dart
+++ Sangsoo/study_9th/lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,14 +7,25 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
+          onPressed: () {},
           child: Text('Get Location'),
         ),
       ),
